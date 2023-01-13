db1f1cfb2b878b6f09a2fbebae4924a4a92fd561
Fatima Zahrae Laaziz <laazizfatimazahrae@gmail.com>
2023-01-13 10:28:43 +0100

    hello world

---
+++ src/observer/Client.java
@@ -0,0 +1,18 @@
+package observer;
+
+public class Client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Subject subject = new Subject(0);
+		Observer binary = new BinaryObserver(subject);
+		Observer octal = new OctalObserver(subject);
+		Observer hexa = new HexaObserver(subject);
+		Thread thread = new Thread(subject);
+		thread.start();
+	}
+
+}
+
