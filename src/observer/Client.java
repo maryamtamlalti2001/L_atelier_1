ec3663e7dfef9b8ad9802e717cfe907d80dea5f2
Your Name <you@example.com>
2023-01-13 11:40:42 +0100

    mery

--- src/observer/Client.java
+++ src/observer/Client.java
@@ -7,11 +7,13 @@ public class Client {
 	 */
 	public static void main(String[] args) {
 		Subject subject = new Subject(0);
-		Observer binary = new BinaryObserver(subject);
+	
 		Observer octal = new OctalObserver(subject);
 		Observer hexa = new HexaObserver(subject);
 		Thread thread = new Thread(subject);
 		thread.start();
+		System.out.print("hey i am just adding this line i am using git ");
+		
 	}
 
 }
